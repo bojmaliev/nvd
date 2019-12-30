56ffb55ff7ab2dc17add9cabe75e7b7748f7aa2f
Martin Bojmaliev <mbojmaliev@gmail.com>
2019-12-30 01:38:56 +0100

    lab05 finished zad1

--- lab05/zad1/index.js
+++ lab05/zad1/index.js
@@ -1,2 +1,22 @@
 
 // Your Vue code here....
+
+let app = new Vue({
+    el: "#app",
+    data:{
+        message:"",
+        has_uppercase:false,
+        has_lowercase:false,
+        has_number:false,
+        has_special:false
+    },
+    methods:{
+        input: function(){
+            this.has_uppercase = this.message.match(/[A-Z]/) !== null;
+            this.has_lowercase = this.message.match(/[a-z]/) !== null;
+            this.has_number = this.message.match(/[0-9]/) !== null;
+            this.has_special = this.message.match(/[!@#$%^&*()/~{}]/) !== null;
+        },
+
+    }
+});
